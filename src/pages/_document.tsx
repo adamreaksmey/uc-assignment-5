0fe5bedfd0d13ea97050908f616719f154121222
adamreaksmey <adaminiature@gmail.com>
2024-02-10 22:25:36 +0700

    init stuff

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,10 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        {/* Not recommended but ill just put it here anyway. */}
+        <title>Adam's bookstore | Home</title>
+      </Head>
       <body>
         <Main />
         <NextScript />
